8651f2d2a6f075d77fbcbe7533cb916491a4d3f3
Eugene The Dream <ek@remote>
2025-03-23 16:45:21 -0700

    Binary releases

--- build_release.sh
+++ build_release.sh
@@ -7,14 +7,14 @@ mkdir -p res/release
 pushd venear-contract
 cargo near build reproducible-wasm
 popd
-cp target/near/venear_contract/venear_contract.wasm res/release/
 
 pushd lockup-contract
 cargo near build reproducible-wasm
 popd
-cp target/near/lockup_contract/lockup_contract.wasm res/release/
 
 pushd voting-contract
 cargo near build reproducible-wasm
 popd
+cp target/near/venear_contract/venear_contract.wasm res/release/
+cp target/near/lockup_contract/lockup_contract.wasm res/release/
 cp target/near/voting_contract/voting_contract.wasm res/release/
